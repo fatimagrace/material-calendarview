517024725d12ee5eabea485284509dc1a5bd80dd
fatimagracecarillo <fatima.g.m.carillo@accenture.com>
2017-03-22 20:17:34 +0800

    Remove Center for drawable

--- library/src/main/java/com/prolificinteractive/materialcalendarview/DayView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/DayView.java
@@ -272,10 +272,10 @@ class DayView extends CheckedTextView {
         final int circleOffset = Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP ? offset / 2 : offset;
 
         if (width >= height) {
-            tempRect.set(offset, 0, radius + offset, height);
+            //tempRect.set(offset, 0, radius + offset, height);
             circleDrawableRect.set(circleOffset, 0, radius + circleOffset, height);
         } else {
-            tempRect.set(0, offset, width, radius + offset);
+            //tempRect.set(0, offset, width, radius + offset);
             circleDrawableRect.set(0, circleOffset, width, radius + circleOffset);
         }
     }
